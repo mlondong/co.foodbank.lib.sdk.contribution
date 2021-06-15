e7cd52074946334e647512ab056da2331eb8605a
mlondong <mauricio.londono@gmail.com>
2021-06-15 12:12:50 -0500

    first commit

---
+++ src/test/java/co/com/foodbank/contribution/co/foodbank/lib/sdk/contribution/ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.foodbank.contribution.co.foodbank.lib.sdk.contribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
